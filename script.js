6ace17e69a85a88f9a13da00d915b8e2a0741df7
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-16 09:55:41 +0530

    update_the_js-code

--- script.js
+++ script.js
@@ -6,24 +6,38 @@ document.addEventListener("DOMContentLoaded", function () {
   let company = document.querySelector("#company");
   let review = document.querySelector("#review");
 
+
+
+
+
+
   let images = [
     "./images/adrian-williams-img.png",
     "./images/ronald-jones-img.png",
     "./images/sherry-jhonson-img.png",
     "./images/wade-warren-img.png",
   ];
+
+
+
   let titles = [
     "Adrian Williams",
     "Ronald Janes",
     "Sherry Jhonson",
     "Wade Warren",
   ];
+
+
+
   let companies = [
     "Infinos Tech",
     "Infinos Tech",
     "Infinos Tech",
     "Infinos Tech",
   ];
+
+
+
   let reviews = [
     "The most important thing I learnt is that nothing is a failure,but what we learn from that is a rich and rewarding experience.",
     "Coming to Startup School is the best thing that has happened to me. I wish every startup in the country should get this opportunity.",
@@ -31,7 +45,13 @@ document.addEventListener("DOMContentLoaded", function () {
     "I am really loving the way how mentors are taking care of us, the way they are explaining big theories with lots of case studies and innovative methods.",
   ];
 
+
+  
+
   let currentImageIndex = 0;
+
+
+
 
   function updateContent() {
     image.src = images[currentImageIndex];
@@ -39,6 +59,8 @@ document.addEventListener("DOMContentLoaded", function () {
     company.innerHTML = companies[currentImageIndex];
     review.innerHTML = reviews[currentImageIndex];
   }
+
+
 
   rightArrow.addEventListener("click", function (e) {
     e.preventDefault();
@@ -49,6 +71,9 @@ document.addEventListener("DOMContentLoaded", function () {
     updateContent();
   });
 
+
+
+
   leftArrow.addEventListener("click", function (e) {
     e.preventDefault();
     currentImageIndex--;
@@ -57,6 +82,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     updateContent();
   });
+
+
+
 
   // Initialize the content with the first image
   updateContent();
